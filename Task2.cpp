c409c7a95a0d6e37ddf9271b87c750da1e240e49
maksim <WalkLU@yandex.ru>
2024-09-19 16:53:32 +0300

    v1

--- Task2.cpp
+++ Task2.cpp
@@ -2,14 +2,20 @@
 
 void pointer(int * pi)
 {
-    for(int i = 0; i < 10; ++i)
+    for(int i = 0;i < 5;++i)
     {
-        std::cout<< *(pi - i) <<" ";
+        int temp = *(pi + i);
+        *(pi + i) = *(pi + 9 - i);
+        *(pi + 9 - i) = temp;        
+    }
+    for(int i = 0;i < 10;++i)
+    {
+        std::cout<<*(pi + i)<<" ";
     }
 }
 
 int main()
 {
-    int numbers[10] {1,2,3,4,5,6,7,8,9,10};    
-    pointer(numbers+9);
+    int numbers[10] {1,2,3,4,5,6,7,8,9,10};       
+    pointer(numbers);
 }
--- Task3.cpp
+++ Task3.cpp
@@ -6,9 +6,9 @@ bool subStr(const char * a, const char * b)
     for(int i = 0; i <= strlen(a) - strlen(b);++i)
     {   
         bool found = true;
-        for(int j = i; j <= strlen(b);++j)
+        for(int j = 0; j < strlen(b);++j)
         {
-            if(*(a + i) != *(b + i))
+            if(*(a + i + j) != *(b + j))
             {
                 found = false;
             }
@@ -25,6 +25,6 @@ int main()
 {
     const char * a = "Hello world!";
     const char * b = "wor";
-    const char * c = "banana";
+    const char * c = "banana";    
     std::cout << std::boolalpha << subStr(a,b) <<" "<<subStr(a,c);
 }
